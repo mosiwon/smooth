e84cdab36f5cc52bdca890a2347cdbaa167ee298
mosi <61589097+mosiwon@users.noreply.github.com>
2023-04-04 19:35:51 +0900

    음악관련
    bug
    - 슬라이더바 이상 적용
    - stop&play 노래가 따른걸로 바뀜

--- source/smooth.py
+++ source/smooth.py
@@ -20,6 +20,8 @@ from collections import Counter
 import pygame
 
 # 카메라 쓰레드
+
+
 class Camera(QThread):
     update = pyqtSignal()
 
@@ -45,14 +47,14 @@ class WindowClass(QMainWindow, from_class):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("Smart Moodlight Speaker")
-
+######### esp 연결 관련 변수 #########   
         self.connect = False
         self.cam = False
 
         # ip address
-        range = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
-        ipRegex = QRegularExpression("^" + range + "\\." + range + "\\." +
-                                     range + "\\." + range + "$")
+        ip_range = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
+        ipRegex = QRegularExpression("^" + ip_range + "\\." + ip_range + "\\." +
+                                     ip_range + "\\." + ip_range + "$")
 
         self.ip_edt.setValidator(QRegularExpressionValidator(ipRegex, self))
         self.port_edt.setValidator(QIntValidator())
@@ -66,28 +68,45 @@ class WindowClass(QMainWindow, from_class):
 
         self.connecting_btn.clicked.connect(self.connecting)
         self.camera.update.connect(self.camUpdate)
-        
+######### esp 연결 관련 변수 #########        
+######### 감정 인식 관련 변수 #########
         self.emotion_mean = 'Neutral'
         # Emotion Recognition
-        self.face_mesh = mp.solutions.face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        self.face_mesh = mp.solutions.face_mesh.FaceMesh(
+            max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
         self.mp_drawing = mp.solutions.drawing_utils
-        self.drawing_spec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+        self.drawing_spec = self.mp_drawing.DrawingSpec(
+            thickness=1, circle_radius=1)
         self.model_name = 'enet_b0_8_best_vgaf'
         self.fer = HSEmotionRecognizer(model_name=self.model_name)
         self.recent_scores = deque(maxlen=15)
         self.emotion_mean = None
         self.emotion_queue = deque(maxlen=500)
         
-        ## whlie문으로 emotion_queue에 Neutral을 300번 넣어주기
+        # emotion_edt가 바뀔때 
+        self.emotion_edt.textChanged.connect(self.emotion_edt_changed)
+######### 감정 인식 관련 변수 #########
+######### 음악 재생 관련 변수 #########
+        # whlie문으로 emotion_queue에 Neutral을 300번 넣어주기
         while len(self.emotion_queue) < 300:
             self.emotion_queue.append('Neutral')
-        
+
         self.btnColorPicker.clicked.connect(self.btnColorPicker_clicked)
         self.btnColorFalse.clicked.connect(self.btnColorModeFalse_clicked)
         self.pickerModeOn = False
+
+        # 음악(30개) (파일이름:musicismylife1~30)불러오기
+        self.music_list = []
+        for i in range(1, 31):
+            self.music_list.append(
+                f'/home/siwon/dev/smooth/data/musicismylife{i}.mp3')
+        self.music_timer = None
+        self.MusicSlider.valueChanged.connect(self.MusicSlider_changed)
+        self.btnMusic.clicked.connect(self.btnMusic_clicked)
+        self.musicison = False
+        self.stopped_position = 0
+        self.musicUpdate(1)
         self.camStart()
-        
-        
 
     def getQImage(self, frame):
         height, width, channel = frame.shape
@@ -109,7 +128,6 @@ class WindowClass(QMainWindow, from_class):
             self.cam = True
             self.connecting_btn.setText("Disconnecting")
             self.cam_label.setText("Cam On")
-
             self.camStart()
 
         # 연결X
@@ -139,7 +157,7 @@ class WindowClass(QMainWindow, from_class):
             image = imutils.resize(image, width=400)
             image = self.emotion_detector(image)
             self.update_emotion_ui()
-            
+
             h, w, c = image.shape
             qImg = QImage(image.data, w, h, w*c, QImage.Format.Format_RGB888)
 
@@ -148,7 +166,6 @@ class WindowClass(QMainWindow, from_class):
                 self.label.width(), self.label.height())
 
             self.label.setPixmap(self.pixmap)
-
 
     def emotion_detector(self, image):
         image_rgb = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -173,80 +190,100 @@ class WindowClass(QMainWindow, from_class):
                 face_img = image_rgb[y1:y2, x1:x2, :]
                 if np.prod(face_img.shape) == 0:
                     continue
-                emotion, scores = self.fer.predict_emotions(face_img, logits=True)
+                emotion, scores = self.fer.predict_emotions(
+                    face_img, logits=True)
 
                 self.recent_scores.append(scores)
                 scores = np.mean(self.recent_scores, axis=0)
                 emotion = np.argmax(scores)
-                emotion_str = self.fer.idx_to_class[emotion]  # emotion을 문자열로 변환
-                self.emotion_queue.append(emotion_str)  # emotion_queue에 문자열 emotion 추가
-                self.emotion_mean = max(set(self.emotion_queue), key=self.emotion_queue.count)
-            
+                # emotion을 문자열로 변환
+                emotion_str = self.fer.idx_to_class[emotion]
+                # emotion_queue에 문자열 emotion 추가
+                self.emotion_queue.append(emotion_str)
+                self.emotion_mean = max(
+                    set(self.emotion_queue), key=self.emotion_queue.count)
+
                 cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
                 min_y = y1 if y1 >= 0 else 10
-                cv2.putText(image, self.fer.idx_to_class[emotion], (x1, min_y), cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 255, 0), thickness=1)
-                
+                cv2.putText(image, self.fer.idx_to_class[emotion], (
+                    x1, min_y), cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 255, 0), thickness=1)
+
         return image
-    
+
     def update_emotion_ui(self):
         # emotion_edt 에 emotion_mean 출력
-        #self.emotion_edt.setText(self.emotion_mean)
-        
+        # self.emotion_edt.setText(self.emotion_mean)
+
         # emotion_mean에 따라 R,G,B 변경
         if self.emotion_mean == 'Anger':
             self.emotion_edt.setStyleSheet("color: rgb(255, 0, 0);")
             self.emotion_edt.setText("분노")
             self.colorUpdate(255, 0, 0, 1)
-            
+            self.musicUpdate(1)
+         
+
         elif self.emotion_mean == 'Contempt':
             self.emotion_edt.setStyleSheet("color: rgb(255, 255, 0);")
             self.emotion_edt.setText("경멸")
             self.colorUpdate(255, 255, 0, 1)
+            self.musicUpdate(2)
+         
 
         elif self.emotion_mean == 'Disgust':
             self.emotion_edt.setStyleSheet("color: rgb(0, 128, 0);")
             self.emotion_edt.setText("혐오")
             self.colorUpdate(0, 128, 0, 1)
+            self.musicUpdate(3)
+          
 
         elif self.emotion_mean == 'Fear':
             self.emotion_edt.setStyleSheet("color: rgb(0, 0, 255);")
             self.emotion_edt.setText("공포")
             self.colorUpdate(0, 0, 255, 1)
+      
+
 
         elif self.emotion_mean == 'Happiness':
             self.emotion_edt.setStyleSheet("color: rgb(255, 192, 203);")
             self.emotion_edt.setText("행복")
             self.colorUpdate(255, 192, 203, 1)
+            self.musicUpdate(5)
+ 
 
         elif self.emotion_mean == 'Neutral':
             self.emotion_edt.setStyleSheet("color: rgb(128, 128, 128);")
             self.emotion_edt.setText("보통")
             self.colorUpdate(128, 128, 128, 1)
+            self.musicUpdate(6)
+
 
         elif self.emotion_mean == 'Sadness':
             self.emotion_edt.setStyleSheet("color: rgb(0, 255, 255);")
             self.emotion_edt.setText("슬픔")
             self.colorUpdate(0, 255, 255, 1)
+            self.musicUpdate(7)
+
 
         elif self.emotion_mean == 'Surprise':
             self.emotion_edt.setStyleSheet("color: rgb(128, 0, 128);")
             self.emotion_edt.setText("놀람")
             self.colorUpdate(128, 0, 128, 1)
-            
-            
-        
+            self.musicUpdate(8)
+
+
     def btnColorPicker_clicked(self):
         color = QColorDialog.getColor()
         if color.isValid():
             self.pickerModeOn = True
-            self.btnColorPicker.setStyleSheet("background-color: {}".format(color.name()))
-            r,g,b = color.getRgb()[:3]
-            self.colorUpdate(r,g,b,0)
-            
+            self.btnColorPicker.setStyleSheet(
+                "background-color: {}".format(color.name()))
+            r, g, b = color.getRgb()[:3]
+            self.colorUpdate(r, g, b, 0)
+
     def colorUpdate(self, r, g, b, mode=1):
-        ## colorpickermodeON이 True일때는 colorpicker에서 받은 색상으로 변경
-        ## mode가 1일때 colorpickermodeon이 true라면 pass
-        ## mode가 0일때 colorpickermodeon이 false라면 pass
+        # colorpickermodeON이 True일때는 colorpicker에서 받은 색상으로 변경
+        # mode가 1일때 colorpickermodeon이 true라면 pass
+        # mode가 0일때 colorpickermodeon이 false라면 pass
         if mode == 1:
             if self.pickerModeOn:
                 pass
@@ -261,10 +298,89 @@ class WindowClass(QMainWindow, from_class):
                 self.B_led_edt.setText(str(b))
             else:
                 pass
-            
+
     def btnColorModeFalse_clicked(self):
         self.pickerModeOn = False
-        self.btnColorPicker.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.btnColorPicker.setStyleSheet(
+            "background-color: rgb(255, 255, 255);")
+        
+    def musicUpdate(self, emotion):
+        if emotion == 1:
+            self.music = self.music_list[0]
+        elif emotion == 2:
+            self.music = self.music_list[1]
+        elif emotion == 3:
+            self.music = self.music_list[2]
+        elif emotion == 4:
+            self.music = self.music_list[3]
+        elif emotion == 5:
+            self.music = self.music_list[4]
+        elif emotion == 6:
+            self.music = self.music_list[5]
+        elif emotion == 7:
+            self.music = self.music_list[6]
+        elif emotion == 8:
+            self.music = self.music_list[7]
+        else:
+            self.music = self.music_list[8]
+            
+
+    def update_MusicSlider(self):
+        # Get the current position of the music playback
+        current_position = pygame.mixer.music.get_pos() // 1000  # Convert to seconds
+
+        # Update the slider's value
+        self.MusicSlider.setValue(current_position)
+
+        # If the music has stopped playing, stop the timer
+        if not pygame.mixer.music.get_busy():
+            self.music_timer.stop()
+
+
+    def play_music(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.music)
+        pygame.mixer.music.play()
+
+        # Stop the previous timer if it exists
+        if self.music_timer is not None:
+            self.music_timer.stop()
+
+        # Start the timer to update the music slider
+        self.music_timer = QTimer()
+        self.music_timer.timeout.connect(self.update_MusicSlider)
+        self.music_timer.start(1000)  # Update every 1000 ms
+
+    def MusicSlider_changed(self, value):
+        # Check if the music is playing
+        if pygame.mixer.music.get_busy():
+            # Pause the music
+            pygame.mixer.music.pause()
+
+            # Set the new position based on the slider's value
+            pygame.mixer.music.set_pos(value)
+
+            # Resume playing the music
+            pygame.mixer.music.unpause()
+         
+    def btnMusic_clicked(self):
+        self.musicison = not self.musicison
+        if self.musicison:
+            pygame.mixer.music.stop()
+            self.stopped_position = pygame.mixer.music.get_pos() // 1000
+            self.music_timer.stop()
+            self.btnMusic.setText("Play")
+        else:
+            self.play_music()
+            pygame.mixer.music.set_pos(self.stopped_position)
+            self.btnMusic.setText("Stop")
+
+            
+            
+    def emotion_edt_changed(self):
+        self.play_music()
+
+
 
 
 if __name__ == "__main__":
